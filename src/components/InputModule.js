ccd27deb889970dd52157bd8f9bdab8dc70f4126
Volodymyr Maksymenko <462960@gmail.com>
2020-12-26 11:39:12 +0300

    Some improvements

--- src/components/InputModule.js
+++ src/components/InputModule.js
@@ -2,33 +2,33 @@ import React, { useState } from "react";
 
 import Paper from "@material-ui/core/Paper";
 import Button from "@material-ui/core/Button";
-import Input from "@material-ui/core/Input";
+import Input from '@material-ui/core/Input';
 
 const InputModule = ({ addTodo }) => {
-  const [name, setName] = useState("");
+    const [name, setName] = useState("");
 
-  const handleInput = (e) => {
-    const text = e.target.value;
-    setName(text.trim());
-  };
+    const handleInput = (e) => {
+        const text = e.target.value;
+        setName(text.trim());
+    };
 
-  const handleSubmit = () => {
-    name && addTodo(name);
-    setName("");
-  };
+    const handleSubmit = () => {
+        name && addTodo(name);
+        setName("");
+    };
 
-  return (
-    <Paper>
-      <ul className="inputs">
-        <li>
-          <Input onChange={handleInput} value={name} fullWidth={true} />
-        </li>
-        <li>
-          <Button onClick={handleSubmit}>Add</Button>
-        </li>
-      </ul>
-    </Paper>
-  );
+    return (
+        <Paper>
+            <ul className="inputs">
+                <li>
+                    <Input onChange={handleInput} value={name} fullWidth={true} />
+                </li>
+                <li>
+                    <Button onClick={handleSubmit}>Add</Button>
+                </li>
+            </ul>
+        </Paper>
+    );
 };
 
 export default InputModule;
--- src/components/ListFiltration.js
+++ src/components/ListFiltration.js
@@ -3,51 +3,51 @@ import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import FormControl from "@material-ui/core/FormControl";
-import { Paper } from "@material-ui/core";
+import { Paper } from '@material-ui/core';
 
 const ListFiltration = ({ filterValue, setFilterValue }) => {
-  const handleChange = (e) => {
-    setFilterValue(e.target.value);
-  };
+    const handleChange = (e) => {
+        setFilterValue(e.target.value);
+    };
 
-  return (
-    <div className="items-wrapper">
-      <Paper>
-        <FormControl component="fieldset">
-          <RadioGroup
-            aria-label="gender"
-            name="gender1"
-            value={filterValue}
-            onChange={handleChange}
-          >
-            <ul className="radio-controls">
-              <li>
-                <FormControlLabel
-                  value="all"
-                  control={<Radio color="primary" />}
-                  label="All"
-                />
-              </li>
-              <li>
-                <FormControlLabel
-                  value="checked"
-                  control={<Radio color="primary" />}
-                  label="Checked"
-                />
-              </li>
-              <li>
-                <FormControlLabel
-                  value="todo"
-                  control={<Radio color="primary" />}
-                  label="To do"
-                />
-              </li>
-            </ul>
-          </RadioGroup>
-        </FormControl>
-      </Paper>
-    </div>
-  );
+    return (
+        <div className="items-wrapper">
+            <Paper>
+                <FormControl component="fieldset">
+                    <RadioGroup
+                        aria-label="gender"
+                        name="gender1"
+                        value={filterValue}
+                        onChange={handleChange}
+                    >
+                        <ul className="radio-controls">
+                            <li>
+                                <FormControlLabel
+                                    value="all"
+                                    control={<Radio color="primary" />}
+                                    label="All"
+                                />
+                            </li>
+                            <li>
+                                <FormControlLabel
+                                    value="checked"
+                                    control={<Radio color="primary" />}
+                                    label="Checked"
+                                />
+                            </li>
+                            <li>
+                                <FormControlLabel
+                                    value="todo"
+                                    control={<Radio color="primary" />}
+                                    label="To do"
+                                />
+                            </li>
+                        </ul>
+                    </RadioGroup>
+                </FormControl>
+            </Paper>
+        </div>
+    );
 };
 
 export default ListFiltration;
